2232259fd60738a4c6bbbd463d3b825375764ded
shivg <docshivaguru@gmail.com>
2016-03-11 14:55:55 -0800

    Adds sql data model

--- db/query.sql
+++ db/query.sql
@@ -5,8 +5,10 @@ GRANT ALL PRIVILEGES ON hireOrbit.* TO 'hireOrbit'@'localhost';
 /*Before running this part you need to login as the gitBegin user (or some user w/ permission on the db)*/
 use hireOrbit;
 
+/* DROP TABLE IF EXISTS indeed_jobs;*/
+
 create table indeed_jobs (
-	internal_id int AUTO_INCREMENT PRIMARY KEY,
+  internal_id int AUTO_INCREMENT PRIMARY KEY,
   jobtitle nvarchar(200),
   company nvarchar(50),
   city nvarchar(50),
@@ -22,6 +24,7 @@ create table indeed_jobs (
   latitude nvarchar(20),
   longitude nvarchar(20),
   jobkey nvarchar(30),
+  UNIQUE (jobkey),
   sponsored bool,
   expired bool,
   indeedApply bool,
@@ -29,6 +32,37 @@ create table indeed_jobs (
   noUniqueUrl bool,
   formattedRelativeTime nvarchar(30),
   onmousedown nvarchar(50)
-	);
+  );
 
+create table saved_searches (
+  internal_id int AUTO_INCREMENT PRIMARY KEY,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  user_id int NOT NULL,
+);
 
+create table jobs_saved_searches (
+  internal_id int AUTO_INCREMENT PRIMARY KEY,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  jobkey_id nvarchar(30) NOT NULL,
+  saved_search_id int NOT NULL,
+);
+
+create table users (
+  internal_id int AUTO_INCREMENT PRIMARY KEY,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  username nvarchar(50) NOT NULL,
+  UNIQUE (username),
+  name nvarchar(100),
+  github_avatar_url nvarchar(100),
+  github_html_url nvarchar(100),
+  github_access_token nvarchar(60),
+  github_refresh_token nvarchar(60),
+);
+
+ALTER TABLE saved_searches ADD CONSTRAINT fk_user_id FOREIGN KEY (user_id) REFERENCES users (internal_id);
+
+ALTER TABLE jobs_saved_searches ADD CONSTRAINT fk_jobkey FOREIGN KEY (jobkey_id) REFERENCES indeed_jobs (jobkey);
+ALTER TABLE jobs_saved_searches ADD CONSTRAINT fk_saved_search_id FOREIGN KEY (saved_search_id) REFERENCES saved_searches (internal_id);
