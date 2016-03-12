5175ebae74a6eed965219f67847a5e988b8bbe25
shivg <docshivaguru@gmail.com>
2016-03-11 20:03:35 -0800

    Working webpack config with react

---
+++ client/components/Note.js
@@ -0,0 +1,3 @@
+import React from 'react';
+
+export default () => <div>Show me the money</div>;
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,51 @@
 var path = require('path');
 var webpack = require('webpack');
+var production = process.env.NODE_ENV === 'production';
+var CleanPlugin = require('clean-webpack-plugin');
 
 var outputPath = __dirname + '/client';
+
+if (production) {
+    plugins = plugins.concat([
+      new CleanPlugin('builds'),
+      
+      new webpack.optimize.DedupePlugin(),
+
+     
+      new webpack.optimize.OccurenceOrderPlugin(),
+
+   
+      new webpack.optimize.MinChunkSizePlugin({
+          minChunkSize: 51200, // ~50kb
+      }),
+
+      new webpack.optimize.UglifyJsPlugin({
+          mangle:   true,
+          compress: {
+              warnings: false, // Suppress uglification warnings
+          },
+      }),
+
+      // This plugins defines various variables that we can set to false
+      // in production to avoid code related to them from being compiled
+      // in our final bundle
+      new webpack.DefinePlugin({
+          __SERVER__:      !production,
+          __DEVELOPMENT__: !production,
+          __DEVTOOLS__:    !production,
+          'process.env':   {
+              BABEL_ENV: JSON.stringify(process.env.NODE_ENV),
+          },
+      })
+
+    ]);
+}
 
 module.exports = {
   entry: './client/init.js',
   output: { path: outputPath, filename: 'bundle.js' },
-  devtool: 'source-map',
+  // plugin: plugins,
+  devtool: 'eval-source-map',
   module: {
     loaders: [
       {
@@ -14,10 +53,11 @@ module.exports = {
         loader: 'babel',
         exclude: [/node_modules/, /typings/, /bower_components/],
         query: {
-          presets: ['es2015', 'react']
+          presets: ['react', 'es2015']
         }
       }
     ]
   },
 
 };
+
