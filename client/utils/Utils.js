bb9062de6a48c6c541fb7b644f30784503b0f271
shivg <docshivaguru@gmail.com>
2016-03-11 17:50:13 -0800

    Indeed api does not support CORS, using jsonp with jquery

--- client/utils/Utils.js
+++ client/utils/Utils.js
@@ -1,38 +1,58 @@
-const $ = require ('jquery')
-const mergeDefault = require('lodash.defaults');
+// const $ = require ('jquery')
+// const mergeDefault = require('lodash.defaults');
+// const Promise = require('bluebird');
+const mergeDefault = _.defaults
+window.indeedCallback = (data) => {
+  $('body').text(data);
+}
 const Utils = {
- 
-  getJobsFromIndeed : function (query, successCb, errorCb) {
+  getClientIP: function () {
+    console.log('jquery is: ', $)
+    return $.ajax({
+      url: "http://jsonip.com/?callback=?",
+      dataType: 'json'
+    });
+  },
+
+  getJobsFromIndeed: function (query, successCb, errorCb) {
+
+    query = query || {q:'software engineer', l: 'san francisco'}
     var queryStr = {
         publisher: 788696528762292, 
-        q: 'software engineer',
-        l: 'san francisco',
         radius: 200,
         format: 'json',
         limit:25,
-        userip: '201.176.34.12',
-        v:2
+        v:2,
     }
-    
     //use query as default and add in defaults from queryStr. query is the determining factor in the resulting object
     mergeDefault(query, queryStr);
 
     var options = {
       url: 'http://api.indeed.com/ads/apisearch',
       type: 'GET',
+      dataType: 'jsonp',
       data: queryStr,
       success: (res) => {
         console.log('Got data from server in getJobsFromIndeed');
-        successCb(res)
+        indeedCallback(JSON.stringify(res.results));
       },
       error: (err) => {
         console.log('Error in getJobsFromIndeed');
-        errorCb(err);
+        // errorCb(err);
       }
   }
-
-    // makes the call with above options
-    $.ajax(options)
+    // makes the call with above options.
+    // Indeed requires the client IP
+    Utils.getClientIP()
+      .done((data) => {
+        options.data.userip = data.ip;
+        $.ajax(options)
+      })
+      .fail((err) => {
+        console.log('Failed to get IP')
+      })
+  }
 }
-
 // module.exports = Utils;
+
+Utils.getJobsFromIndeed(null)
