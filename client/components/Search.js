b75d7bf91af613852f14def5fd5d6f863d214632
shivg <docshivaguru@gmail.com>
2016-03-16 19:10:32 -0700

    Search displays data from Indeed API

--- client/components/Search.js
+++ client/components/Search.js
@@ -1,20 +1,40 @@
 import React from 'react';
-
+import Utils from '../utils/Utils';
+import JobsList from './JobsList';
 
 class Search extends React.Component {
   constructor(props){
     super(props);
 
     this.state = {
-      searchVal: 'software engineer'
+      searchVal: 'software engineer',
+      jobs: []
     }
   }
 
   handleSearch(e){
     console.log(e.target.value);
-    this.setState({searchVal: e.target.value})
+    this.setState({searchVal: e.target.value});
+    if (e.charCode === 13 || e.keyCode === 13){
+      this.getSearchJobs()
+    }
   }
 
+
+  getSearchJobs(query){
+    query = query || this.state.searchVal;
+    Utils.getJobsFromIndeed({q:query}, 
+      function (res) {
+        console.log('inside jobs setState');
+        this.setState({jobs: res.results});
+        console.log(this.state)
+       }.bind(this), 
+      console.log.bind(console));
+  }
+
+  componentDidMount(){
+    this.getSearchJobs('software engineer');
+  }
   render(){
     // overlay with advanced options
     const AdvancedSearch = () => 
@@ -24,7 +44,9 @@ class Search extends React.Component {
       <div>
         <input name='search' type='text' value={this.state.searchVal} 
         className='search-bar' placeholder='Search for jobs' onChange={this.handleSearch.bind(this)} />
-        <AdvancedSearch />
+        <AdvancedSearch /><br/>
+
+        <JobsList jobs={this.state.jobs}/>
       </div>
       )
   }
