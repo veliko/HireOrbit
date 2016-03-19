15dd34e6bd667d4a7053ade71a9bc08e4cd7699a
shivg <docshivaguru@gmail.com>
2016-03-18 18:24:56 -0700

    Kanban jobs update to the workflow table without rank for now

--- server/routes.js
+++ server/routes.js
@@ -1,5 +1,6 @@
 const githubPassportHandler = require('./Controllers/authController');
 const searchController = require('./Controllers/searchController');
+const kanbanController = require('./Controllers/kanbanController');
 
 module.exports = function (app) {
   app.get('/api/searches/:id', searchController.getSavedSearch);
@@ -7,6 +8,12 @@ module.exports = function (app) {
   app.get('/api/searches', searchController.getAllSearches);
 
   app.post('/api/searches', searchController.saveSearch);
+
+  app.post('/api/kanban/add', kanbanController.addJobsToKanban);
+
+  app.get('api/kanban', kanbanController.getKanban);
+
+  app.delete('/api/kanban/:job', kanbanController.deleteJobFromKanban)
 
   // handler for /auth/github & /logout =>
   //routes for passport github OAuth2 login and sessions are here
