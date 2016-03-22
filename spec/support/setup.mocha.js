a7314d6dc32418ba11183f71c389df8d7c49c997
Daniel Kim <dkim2707@gmail.com>
2016-03-22 08:42:12 -0700

    Committing working state before pull request

--- spec/support/setup.mocha.js
+++ spec/support/setup.mocha.js
@@ -12,6 +12,7 @@ import chaiJquery from 'chai-jquery';
 // Set up testing environment to run like a browser in the command line
 global.document = jsdom.jsdom('<!doctype html><html><body></body></html>');
 global.window = global.document.defaultView;
+global.navigator = global.window.navigator;
 const $ = jquery(global.window);
 
 // build 'renderComponent' helper that should render a given react class
