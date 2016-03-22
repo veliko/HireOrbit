67a394e3386cc4698ae2629229f40e6641639e2d
Daniel Kim <dkim2707@gmail.com>
2016-03-22 02:04:42 -0700

    Add testing component files, add reducer testing for reducersCard, reducersFilter, reducerSearch, and reducersSave of unknown types

--- client/reducers/reducer_saved_searches.js
+++ client/reducers/reducer_saved_searches.js
@@ -4,10 +4,10 @@ import update from 'react-addons-update';
 
 export default function (state = [], action) {
   switch(action.type){
-    
+
     case actions.FETCH_SAVED_SEARCHES:
       return action.payload.savedSearches || [];
-    
+
     default: return state;
   }
 }
--- spec/reducers/reduce.test.js
+++ spec/reducers/reduce.test.js
@@ -1,30 +1,44 @@
 import ActionType from '../../client/actions/index';
 import reducersCard from '../../client/reducers/reducer_cards';
 import reducersFilter from '../../client/reducers/reducer_filter';
+import reducerSearch from '../../client/reducers/reducer_current_search';
+import reducersSave from '../../client/reducers/reducer_saved_searches';
+import { actions } from '../../client/constants';
 import index from '../../client/reducers/index';
+import { expect } from '../support/setup.mocha.js'
 
+
+describe('Reducers', () => {
+let newState;
+let action = { type: 'unknown'};
 describe('Reducer::reducersCard', () => {
-  it('returns the INITIAL_STATE', () => {
-    // setup
-    let action = { type: 'unknown' };
-    // execute
-    let newState = reducersCard(undefined, { type: 'unknown' });
+  it('Should handle action of unknown type', () => {
+    newState = reducersCard(undefined, action);
+    expect(newState).to.be.instanceOf(Array);
+  });
+});
 
-    // returns the initial state, not an empty array.
-    expect(newState).to.deep.not.equal([]);
+describe('Reducer::reducersFilter', () => {
+  it('Should handle action of unknown type', () => {
+    newState = reducersFilter(undefined, action);
+    expect(newState).to.be.a('string');
+  });
+});
+
+describe('Reducer::reducerSearch', () => {
+  it('Should handle action of unknown type', () => {
+    newState = reducerSearch(undefined, action);
+    expect(newState.results).to.be.instanceOf(Array);
+  });
+});
+
+describe('Reducer::reducersSave', () => {
+  it('Should handle action of unknown type', () => {
+    newState = reducersSave(undefined, action);
+    expect(newState).to.be.instanceOf(Array);
   });
 });
 
 
 
-describe('Reducer::reducersCard', () => {
-  it('returns the INITIAL_STATE', () => {
-    // setup
-    let action = { type: 'unknown' };
-    // execute
-    let newState = reducersCard(undefined, { type: 'unknown' });
-
-    // returns the initial state, not an empty array.
-    expect(newState).to.deep.not.equal([]);
-  });
 });
