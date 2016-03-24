0a3e590e711af03a270f7b08c58ab1f8b32d08d6
Daniel Kim <dkim2707@gmail.com>
2016-03-23 18:57:58 -0700

    Add dataVisual state to the rootReducer

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -3,12 +3,14 @@ import cardsReducer from './reducer_cards';
 import filterReducer from './reducer_filter';
 import currentSearchReducer from './reducer_current_search';
 import savedSearchesReducer from './reducer_saved_searches';
+import dataVisReducer from './reducer_data_vis';
 
 const rootReducer = combineReducers({
   cards: cardsReducer,
   filter: filterReducer,
   currentSearch: currentSearchReducer,
-  savedSearches: savedSearchesReducer
+  savedSearches: savedSearchesReducer,
+  dataVisual: dataVisReducer
 });
 
 export default rootReducer;
