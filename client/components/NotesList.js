5514affb03d9200b237d7eea24b041ca57b22b28
veliko <vcm060@gmail.com>
2016-03-29 11:41:23 -0700

    Implement notes strikethrough upon clicking a note.

--- client/components/NotesList.js
+++ client/components/NotesList.js
@@ -18,7 +18,7 @@ class NotesList extends Component {
   handleEnterPress(e) {
     if (e.key === "Enter") {
       let notes = this.props.notes || [];
-      notes = notes.concat([this.state.currentNote]);
+      notes = notes.concat([[this.state.currentNote, false]]);
       this.setState({
         currentNote: ''
       });
@@ -32,15 +32,24 @@ class NotesList extends Component {
     this.props.updateCardNotes(this.props.card_id, notes);
   }
 
+  toggleStrikeThrough(noteIndex) {
+    let notes = [...this.props.notes];
+    notes[noteIndex] = [notes[noteIndex][0], notes[noteIndex][1]];
+    notes[noteIndex][1] = !notes[noteIndex][1];
+    this.props.updateCardNotes(this.props.card_id, notes);
+  }
+
   render() {
     let notes = <span className="card__list__entry">"You notes will appear here"</span>;
     if (this.props.notes && this.props.notes.length > 0) {
       notes = this.props.notes.map((note, noteIndex) => {
+        console.log("note contents: ", note);
         return (
-          <div className="card__list__entry" key={`${note}${noteIndex}`}>
+          <div className="card__list__entry" key={`${note[0]}${noteIndex}`}>
             <RemoveButton removeTarget={noteIndex} 
                           removeAction={this.handleNoteRemove.bind(this)} />
-            <span>{(note)}</span>
+            <span style={JSON.parse(note[1])  ? {textDecoration: "line-through"} : {textDecoration: "none"} }
+                  onClick={() => {this.toggleStrikeThrough.bind(this, noteIndex)()}}>{(note[0])}</span>
           </div>
         );
       });
